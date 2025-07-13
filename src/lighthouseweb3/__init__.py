ed8e68ad1d0c8162b3f8b613f14ae2147be33c7a
AnonO6 <21ucs043@gmail.com>
2025-07-13 17:26:51 +0000

    feat:added doc string

--- src/lighthouseweb3/__init__.py
+++ src/lighthouseweb3/__init__.py
@@ -228,8 +228,13 @@ class Lighthouse:
 
     @staticmethod
     def getAccessCondition(cid: str):
+        """
+        Retrieves the access condition for a given content identifier (CID).
+
+        :param cid: str, Content Identifier for the data to be retrieved
+        :return: dict, A dictionary containing the access condition
+        """
         try:
             return getAccessCondition.get_access_condition(cid)
         except Exception as e:
             raise e
-
